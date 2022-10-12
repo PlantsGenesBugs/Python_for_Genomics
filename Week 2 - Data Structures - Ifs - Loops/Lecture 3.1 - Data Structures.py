b46832eda1e4198f80561565b4e1b725fef57119
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-12 22:00:42 +0100

    Create Lecture 3.1 - Data Structures.py

---
+++ Week 2 - Data Structures - Ifs - Loops/Lecture 3.1 - Data Structures.py
@@ -0,0 +1,9 @@
+# Other types of data structures (beyond numbers and strings)
+
+# Lists - an ordered set of values; enclosed in square brackets
+list = ["gene", 5.16e-08, 0.000138511, "a string of words"]
+list[1]
+#5.16e-08
+list[-1]
+#'a string of words'
+
