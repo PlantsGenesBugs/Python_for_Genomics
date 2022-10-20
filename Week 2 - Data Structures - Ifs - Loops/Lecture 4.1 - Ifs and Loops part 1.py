0afb567027eec2ae0b1ce6d2b06cc82fbe68c864
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-20 22:48:47 +0100

    Update Lecture 4.1 - Ifs and Loops part 1.py

--- Week 2 - Data Structures - Ifs - Loops/Lecture 4.1 - Ifs and Loops part 1.py
+++ Week 2 - Data Structures - Ifs - Loops/Lecture 4.1 - Ifs and Loops part 1.py
@@ -1,3 +1,12 @@
 # Ifs and loops - decision making and repetition
 
+# Example of an if statement
+dna = input("Enter DNA sequence:")
+#Enter DNA sequence:
+agcgcggtatatatatgcnccann
+
+if "n" in dna:
+  nbases = dna.count("n")
+  print("DNA sequence has %d undefined bases" % nbases)
+#DNA sequence has 3 undefined bases
 
