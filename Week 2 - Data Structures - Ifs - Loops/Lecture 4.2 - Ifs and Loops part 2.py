402eb69f2a8065d3dd9c3140d9f19f35f9b9c797
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-22 13:18:08 +0100

    Update Lecture 4.2 - Ifs and Loops part 2.py

--- Week 2 - Data Structures - Ifs - Loops/Lecture 4.2 - Ifs and Loops part 2.py
+++ Week 2 - Data Structures - Ifs - Loops/Lecture 4.2 - Ifs and Loops part 2.py
@@ -11,10 +11,21 @@ dna = input("Enter DNA sequence:")
 
 pos = dna.find("gt", 0) #position of donor splice site starting from 0 index (first letter in sequence)
 
-while pos>-1:   #or while pos>=0; keeps on running for each letter in the sequence until finished/condition is breached/the position is -1
+while pos>-1:        #or while pos>=0; keeps on running for each letter in the sequence until finished/condition is breached/the position is -1
     print("Donor splice site candidate at position %d" %pos)
     pos=dna.find("gt", pos+1)
 
     
 # Usage of the for loop. Iterates over the items of a sequence in the order that they appear in the sequence
 
+# Creat list of DNA motifs
+motifs=["attccgt", "agggggtttttcg", "gtagc"]
+for m in motifs:      #for each element in this list
+    print(m, len(m))  #print out the value for that element and its length
+
+#attccgt 7
+#agggggtttttcg 13
+#gtagc 5    
+    
+    
+    
