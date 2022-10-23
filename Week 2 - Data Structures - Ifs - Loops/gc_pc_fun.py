756bf52f08b7e7d0c29e011d1a78947d4b660b38
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-23 12:30:00 +0100

    Delete gc_pc_fun.py

--- Week 2 - Data Structures - Ifs - Loops/gc_pc_fun.py
+++
@@ -1,5 +0,0 @@
-def gc(dna):
-	"this function computes the GC percentage of a dna sequence"
-	nbases = dna.count("n")+dna.count("N")
-	gcpercent = float(dna.count("c")+dna.count("C")+dna.count("g")+dna.count("G"))*100.0/(len(dna)-nbases)
-	return gcpercent
