4a9906a804a1897aaf88a0bcb8b674a7e2a90e30
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-24 21:57:16 +0100

    Create Lecture 7.1 - Communicating with the Outside Part 1.py

---
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.1 - Communicating with the Outside Part 1.py
@@ -0,0 +1,7 @@
+# Reading data from files instead of typing it in
+
+# Open the file; do something to the file; close the file
+
+# Opening files
+open(filename, mode)
+f = open('myfile', 'r')         # Open myfile in reading mode (default; can be omitted)
