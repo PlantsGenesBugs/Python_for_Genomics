773aec537e781e68be5ea8e68808b7c9630d7ed1
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-25 18:28:46 +0100

    Create Lecture 7.2 - Communicating with the Outside Part 2.py

---
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.2 - Communicating with the Outside Part 2.py
@@ -0,0 +1,5 @@
+# How to read and process FASTA files in Python
+# Standard DNA sequence format; one or more sequences
+
+# Example: Build a dictionary containing all the sequences from a FASTA file
+
