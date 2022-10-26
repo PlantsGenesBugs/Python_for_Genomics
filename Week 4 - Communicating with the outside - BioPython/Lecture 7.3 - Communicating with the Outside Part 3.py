12154dec9c963ca07306c94c8c5f751daeff4f8c
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-26 21:19:35 +0100

    Create Lecture 7.3 - Communicating with the Outside Part 3.py

---
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.3 - Communicating with the Outside Part 3.py
@@ -0,0 +1,8 @@
+# How to retrieve data from a dictionary that you built:
+
+# Remember the .keys() and .values() methods from lecture 3.2
+# Can also retrieve key + value using .items() method and looping through the whole dictionary
+
+for name, seq in seqs.items():
+    print(name, seq)            # Because my example file has large sequences, this comes out as a very big block of data!
+
