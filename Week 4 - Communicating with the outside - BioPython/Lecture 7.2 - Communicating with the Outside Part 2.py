afc088de450430aef4badd2458dc3d515b31a2f3
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-26 21:13:44 +0100

    Code - how to make a dictionary from a FASTA file

--- Week 4 - Communicating with the outside - BioPython/Lecture 7.2 - Communicating with the Outside Part 2.py
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.2 - Communicating with the Outside Part 2.py
@@ -2,4 +2,61 @@
 # Standard DNA sequence format; one or more sequences
 
 # Example: Build a dictionary containing all the sequences from a FASTA file
+# Create FASTA file by doing a BLAST search at NCBI, select sequences of interest and download as full FASTA format. The file I used is available in this repository.
+# Build dictionary with gene ID as key and sequence itself as value
+
+# Pseudocode:
+#open file
+#read in one line at a time
+#ask if it is a header or not
+#if header - get sequence name
+#store sequence name as key
+#if NOT header, then update sequence in dictionary (at current key spot)
+#at end of sequence - are there any more lines? continue. If not, break.
+
+try:
+    f=open("myfasta.txt")
+except IOError:
+    print("File does not exist")
+    
+# Main loop of program:
+# First set up empty dictionary
+seqs = {}
+
+# Go through line by line
+# Discard the newline at the end (if any) (anything like returns or anything else)
+
+for line in f:
+  line = line.rstrip()
+
+# Test the line for the presence of > which indicates a gene ID line
+# If present (true), split the line into individual words, and look at the first element of that list [0] - that is the name. Then slice the > sign out [1:]
+# Initialise a new entry for the dictionary with the name specified; fill the sequence in next
+
+  if line.startswith(">"):
+    words=line.split()
+    name=words[0][1:]
+    seqs[name]=""
+
+# When not a header line, append the sequence on the current line to the entry just initiated; append to an existing entry
+  else:
+    seqs[name] = seqs[name]+line
+#f.close()            Adding this to the loop gives a syntax error, so I have excluded it from my code; manually close the file "f"
+
+# Note: if you have previously opened and read the file, make sure you return the current position to 0 by running f.seek(0)
+
+# Check the length of the dictionary
+len(seqs)
+#6
+
+# Check the keys of the dictionary
+list(seqs.keys())
+#['KY549441.1', 'MH598906.1', 'HF543599.2', 'KX752088.1', 'NC_046568.1', 'MF035968.1']
+
+
+
+
+
+
+
 
