0a05e0441af0da9b211d70820a2d3cab2ab7b45e
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-10-29 18:00:31 +0100

    Adding more clarifying notes about sys module

--- Week 4 - Communicating with the outside - BioPython/Lecture 7.3 - Communicating with the Outside Part 3.py
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.3 - Communicating with the Outside Part 3.py
@@ -13,7 +13,7 @@ for name, seq in seqs.items():
 import sys
 print(sys.argv)
 
-# import sys at the top of your programme file. You can print out the arguments at any point in the program code by using print(sys.argv)
+# "import sys" has to appear at the top of your programme file ("processfasta.py" in this instance). You can print out the arguments at any point in the program code by using print(sys.argv)
 #Example code:
 
 #!/usr/bin/env python3
@@ -29,11 +29,23 @@ try:
 except IOError:
 	print("File %s does not exist" % filename)		# Note usage of % operator
 
+# To test if this works, you can edit the above code by adding "print(filename)" at the end. Then run the Python programme from the terminal as shown below and check that the output is "fasta.txt". The file fasta.txt is available in this repository
+
+> python processfasta.py fasta.txt
+#fasta.txt
+
+# Also test that the IOError statement will work
+> python processfasta.py smaug.txt
+# File smaug.txt does not exist
+
     
 # When using a function where some arguments are required and others are optional: the getopt module can help with arguments of sys.argv; note this is for running code/files from the COMMAND LINE
 
 # Example: process FASTA sequences but only add those longer than 250 bp to the dictionary
->processing.py -l 250 myfile.fa
+>python processfasta.py -l 250 myfile.fa		# Running the programme "processing.py" on the file "myfile.fa" specifying a length argument of 250
+
+
+
 
 
 
