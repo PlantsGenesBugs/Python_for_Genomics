ef96739912e29b74e6b6e79757a5a9b39f067dcc
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-11-06 15:25:12 +0000

    Create Lecture 8 - BioPython.py

---
+++ Week 4 - Communicating with the outside - BioPython/Lecture 8 - BioPython.py
@@ -0,0 +1,5 @@
+# BioPython
+# See http://www.biopython.org
+# Includes parsers for various bioinformatics file formats, access to online services, and interfaces to programmes like BLAST, ClustalOmega etc
+
+
