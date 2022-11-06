3b3ffd2eae8a86a0beb0216d90f6bcd3b32a8138
AnnekeP <68586416+PlantsGenesBugs@users.noreply.github.com>
2022-11-06 15:18:53 +0000

    Final notes on using subprocess module

--- Week 4 - Communicating with the outside - BioPython/Lecture 7.3 - Communicating with the Outside Part 3.py
+++ Week 4 - Communicating with the outside - BioPython/Lecture 7.3 - Communicating with the Outside Part 3.py
@@ -13,7 +13,9 @@ for name, seq in seqs.items():
 import sys
 print(sys.argv)
 
-# "import sys" has to appear at the top of your programme file ("processfasta.py" in this instance). You can print out the arguments at any point in the program code by using print(sys.argv). However, the POINT is to have a way for the programme to ACCESS the specified values in the arguments in order to allow it to run (e.g. which file to open, which values to assign to variables in the function)
+# "import sys" has to appear at the top of your programme file ("processfasta.py" in this instance). You can print out the arguments at any point in the program
+# code by using print(sys.argv). However, the POINT is to have a way for the programme to ACCESS the specified values in the arguments in order to allow it to run
+# (e.g. which file to open, which values to assign to variables in the function)
 #Example code:
 
 #!/usr/bin/env python3
@@ -29,7 +31,8 @@ try:
 except IOError:
 	print("File %s does not exist" % filename)		# Note usage of % operator
 
-# To test if this works, you can edit the above code by adding "print(filename)" at the end. Then run the Python programme from the terminal as shown below and check that the output is "fasta.txt". The file fasta.txt is available in this repository
+# To test if this works, you can edit the above code by adding "print(filename)" at the end. Then run the Python programme from the terminal as shown below and
+# check that the output is "fasta.txt". The file fasta.txt is available in this repository
 
 print(filename)		# add this to above code and run as below
 
@@ -44,7 +47,8 @@ print(filename)		# add this to above code and run as below
 # When using a function where some arguments are required and others are optional: the getopt module can help with arguments of sys.argv; 
 
 # Example: process FASTA sequences but only add those longer than 250 bp to the dictionary
->python processfasta.py -l 250 myfile.fa		# Running the programme "processing.py" on the file "myfile.fa" specifying a length argument of 250, where 							   # the length argument is optional
+>python processfasta.py -l 250 myfile.fa		# Running the programme "processing.py" on the file "myfile.fa" specifying a length argument of 250, where
+							# the length argument is optional
 
 # First you have to tell the programme what to do with the arguments that will be specified when executing the programme from the command line:
 
@@ -91,7 +95,7 @@ seqlen=0
 for k,v in o:					# name of arg = k, value assigned to arg = v
 	opts[k]=v				# add the value v to the k'th name in the opts dictionary
 if "-h" in opts.keys():				# after creating the dictionaty, if the user has specified -h (which will be present as a key in the dictionary, then
-	usage()				      # run the usage function defined earlier which prints out a description of the programme
+	usage()				        # run the usage function defined earlier which prints out a description of the programme
 	sys.exit()				# exit after printing help (quit programme without running anything else)
 if len(a) < 1:					# if user has not specified anything after calling the programme	
 	usage()					# initialise the usage function that will print out the message above; then close programme with message
@@ -121,6 +125,30 @@ if "-l" in opts.keys():				# if the user has specified an optional length argume
 >>> sys.stdin.read()	# call the read method; Python waits for input from keyboard
 a line
 another line		# To show you are done with input, Ctrl+D
+
+# Note that on MacOS (perhaps elsewhere) the read output is truncated to 6 characters of each line, and the line break is therefore not shown
+# In order to repeat the sys.stdin.read() command, you first have to move the location of where the input will be read by running sys.stdin.seek(0)
+
+# Interfacing with External Programs
+# You can take output from a Python programme and pass it onto an external programme (which can be in any programming language potentially); e.g. pre-processing
+# some sequences and then handing them over to an analysis programme (TopHat, Cufflinks)
+# This is done by handing the output to the operating system of the computer, which in turn feeds it into the external programme. You specify this path
+# in your script. You can also specify the return of the output from the external programme (although this was not covered in this lecture).
+# You could further use the output from the external programme and pipe it back into your original programme for further processing OR
+# get the output from the external programme and pass it onto another external programme for further processing.
+
+# To pass output on/call external programmes, import the subprocess module (for more info see: https://docs.python.org/3/library/subprocess.html)
+import subprocess
+subprocess.call(["ls", "-l"])		# This calls the ls function from system, with "-l" as argument
+
+# A more realistic example of this:
+subprocess.call(["tophat", "genome_mouse_idx", "PE_reads_1.fq.gz", "PE_reads_2.fq.gz"]) 
+# You are providing a list of arguments as the first argument to the call() function. By convention in the system environment, the first item is the 
+# programme you are calling (executable name). The rest of the arguments in the list appear in the order in which tophat require them.
+# In this case the argument "genome_mouse_idx" specifies the genome that should be used for sequence alignment. The other arguments are the sequences to be used.
+# Tophat will be using sys.argv to get the arguments that you are providing.
+# The point of this lecture is to explain how programs talk to each other via the standard streams provided by the operating system (i.e. communication outside
+# of the specific program that you are using).
 
 
 
